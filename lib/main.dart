9d683c3c84598a735b33f8431d27b102b644e830
kaneshwar-kamal <kaneshwarkamal06@gmail.com>
2026-01-01 21:32:56 +0530

    in mian.dart made the app to be in portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/services.dart'; //Funcionality to lock the scree orientation
+
 
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -12,45 +14,47 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kDarkColorScheme,
-      ),
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 198, 242, 255),
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]).then((_) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
         ),
-        cardTheme: CardThemeData().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin:EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 198, 242, 255),
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardThemeData().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin:EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+              foregroundColor: kColorScheme.onPrimaryContainer,
+            )
+          ),
+          textTheme: TextTheme().copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 23,
+            )
           ),
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-            foregroundColor: kColorScheme.onPrimaryContainer,
-          )
-        ),
-        textTheme: TextTheme().copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: kColorScheme.onSecondaryContainer,
-            fontSize: 23,
-          )
+        themeMode: ThemeMode.system,
+        home: Expenses(
+          onAddExpense: (expense) {},
         ),
       ),
-      themeMode: ThemeMode.system,
-      home: Expenses(
-        onAddExpense: (expense) {},
-      ),
-    ),
-  );
+    );
+  });
 }
